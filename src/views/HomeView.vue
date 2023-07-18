b28b9f91177e9f651a2b72b41a106de353e28eb5
ProphetRen <master.one228@ya.ru>
2023-07-18 16:53:59 +0300

    Added main logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top-block :selectedUserItems="selectedUserItems" :selectedItem="selectedItem"/>
+    <bottom-block :userItems="userItems" :items="items"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import BottomBlock from "@/components/BottomBlock";
+import TopBlock from "@/components/TopBlock";
+import {mapState} from "vuex";
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {BottomBlock, TopBlock},
+  computed: {
+    ...mapState(['selectedUserItems', 'selectedItem', 'items', 'userItems'])
   }
 }
 </script>
+
+<style lang="scss">
+.container {
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  border: 2px solid black;
+  gap: 10px;
+  >div {
+    padding: 5px;
+    border: 1px solid black;
+  }
+}
+</style>
